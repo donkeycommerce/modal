b7ae82989825a46eed13cd75f2bbc777ed07f235
danieletulone <danieletulone.work@gmail.com>
2020-10-01 15:24:38 +0200

    Add Swal adapter and adapter interface

---
+++ src/Adapters/SwalAdapter.ts
@@ -0,0 +1,47 @@
+import { ModalType } from '../types'
+import Modal from "../Modal"
+import ModalAdapterInterface from "./ModalAdapterInterface"
+import Swal, { SweetAlertOptions, SweetAlertResult }  from 'sweetalert2'
+
+class SwalAdapter implements ModalAdapterInterface {
+    public close(instance: Modal) {
+        //
+    }
+
+    public show(instance: Modal, extraConfig: any = {}) {
+        let config: SweetAlertOptions = {
+            title: instance.getTitle(),
+            html: instance.getMessage()
+        }
+
+        if (instance.getType() == ModalType.TOAST) {
+            config.toast = true
+        }
+
+        if (instance.getScope() != undefined) {
+            config.icon = instance.getScope()
+        }
+
+        if (instance.getCancelCallback() != undefined) {
+            config.cancelButtonColor = '#d33'
+            config.showCancelButton = true
+            config.cancelButtonText = instance.getCancelButtonText()        
+        }
+
+        if (instance.getSuccessCallback() != undefined) {
+            config.confirmButtonText = instance.getSuccessButtonText()
+        }
+
+        Swal.fire(Object.assign(config, extraConfig)).then((res: SweetAlertResult) => {
+            if (res.value && instance.getSuccessCallback() != undefined) {
+                instance.getSuccessCallback()()
+            } else {
+                if (instance.getCancelCallback() != undefined) {
+                    instance.getCancelCallback()()
+                }
+            }
+        })
+    }
+}
+
+export default SwalAdapter
