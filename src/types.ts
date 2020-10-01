aec90302b2cb20d8d4d37c869293b40ba718aaed
danieletulone <danieletulone.work@gmail.com>
2020-10-01 15:24:14 +0200

    Add types files

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export enum ModalType {
+    MESSAGE = 'message',
+    TOAST   = 'toast',
+}
+
+export enum ModalScope {
+    WARNING = 'warning',
+    SUCCESS = 'success',
+    ERROR   = 'error'
+}
+
+export default {
+    ModalScope,
+    ModalType
+}
